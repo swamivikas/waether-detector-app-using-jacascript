3f285bbae14d7f4b992e05dfadf655ec9cd5633f
vikas swami <swamivikas2004@gmail.com>
2024-07-11 14:59:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("search-button");
+const input = document.getElementById("city-id");
+
+const cityName = document.getElementById("cityname");
+const localTime = document.getElementById("localtime");
+const temp = document.getElementById("temp_c");
+async  function getdata(cityname){
+
+      const  promise = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=bf2772c6f14d4a789bb72413241107&q=${cityname}&aqi=yes`
+      );
+
+      return await promise.json();
+}
+button.addEventListener("click", async () => {
+    console.log(input.value);
+    const value = input.value;
+    const result = await getdata(value);
+    console.log(result);
+    cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
+    localTime.innerText = `${result.location.localtime}`;
+    temp.innerText = `${result.current.temp_c}`;
+
+});
